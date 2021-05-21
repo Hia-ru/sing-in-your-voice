ce3b463b20579e3e8e605f6701f3f21bcf564120
ysally77 <80692304+ysally77@users.noreply.github.com>
2021-05-21 18:59:26 +0900

    Update vocal.py

--- vocal.py
+++ vocal.py
@@ -1,28 +1,3 @@
-def vocal_extract (vocal,sr) #그냥 무시
-
-1차시도
-
-def audio_analysis(self,path,name):
-	audio, sampling_rate = librosa.load(path,mono=True)
-	sum = 0
-	count = 0 
-	arr = []
-	blocks = 270
-	one_block_data = int(len(audio)/270)
-	w = open(path+".txt",'w')
-	for element in audio:
-		count+=1
-		sum+=abs(element)
-		if(count>=one_block_data):
-			data = int(round(((sum)/one_block_data),3)*300)
-			arr.append(data)
-			count = 0
-			sum = 0
-			w.write(str(data)+"\n")
-		print(arr)
-		w.close()
-
-2차시도
 
 def vocal_extract(wav_file)
 	#MFCC
